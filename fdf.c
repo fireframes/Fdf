6bb194ad4fb1c91d77dcfad3078cca15522038fb
Maksim Maksimov <mmaksimo@z2t3c2.42bangkok.com>
2024-05-04 21:17:00 +0700

    sd

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: mmaksimo <mmaksimo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:43:59 by mmaksimo          #+#    #+#             */
-/*   Updated: 2024/05/02 22:37:33 by mmaksimo         ###   ########.fr       */
+/*   Updated: 2024/05/04 20:25:52 by mmaksimo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
 #include "libft.h"
 #include "MLX42/include/MLX42/MLX42.h"
 
-#define WIDTH 1600
-#define HEIGHT 900
+#define WIDTH 3200
+#define HEIGHT 1800
 #define M_PI 3.14159265358979323846
 
 typedef struct	map_pnt
@@ -59,140 +59,96 @@ static void	ft_error(void)
 
 static void	iso_point(int *x, int *y, int *z, ctrl_param_t *control)
 {
-	double angle = M_PI / 4.0 * control->rotate;
-	int x_tmp;
+	double	angle;
+	int		x_tmp;
+	int		shift_x;
+	int		shift_y;
 
-	int shift_x = WIDTH/2 + control->transpose_x;
-	int shift_y = HEIGHT/4 + control->transpose_y;
-
+	shift_x = WIDTH / 2 + control->transpose_x;
+ 	shift_y = HEIGHT / 4 + control->transpose_y;
+	angle = M_PI / 4 * control->rotate;
 	x_tmp = (int) round((*x - *y) * cos(angle));
 	*y = (int) round((*x + *y) * sin(angle) - *z) + shift_y;
 	*x = x_tmp + shift_x;
-
 }
-/*
-static void	crop_outside(int *x, int *y, double dx_i, double dy_i)
+uint32_t shift_color(uint32_t color, int z0, int z1)
 {
 
-	double a = dy_i / dx_i; // SLOPE
-	double b = *y - *x * a; // INTERCEPT
-	
-	// printf("a:	%lf\t", a);
-	// printf("b:	%lf\n", b);
-	
-	unsigned int yi = 0;
-	unsigned int xi = 0;
-	
-	// printf("before x:	%d\t", *x);
-	// printf("before y:	%d\n", *y);
-	
-	if (*y > HEIGHT)
-	{
-		yi = HEIGHT - 1;
-		xi = (unsigned int) round((yi - b) / a);
-	}
-	else if (*y < 0)
-		xi = (unsigned int) round((yi - b) / a);
-	
-	if (*x > WIDTH)
-	{
-		xi = WIDTH - 1;
-		yi = (unsigned int) round(a * xi + b);
-	}
-	else if (*x < 0)
-		yi = (unsigned int) round(a * xi + b);
-	
-	*x = xi;
-	*y = yi;
-	
-}
-*/
-static void crop_outside(int *_x, int *_y, double dx_i, double dy_i)
-{
-    double a = dy_i / dx_i; // SLOPE
-    double b = *_y - *_x * a; // INTERCEPT
+	uint32_t r_mask = 0x00FFFFFF;
+	uint32_t g_mask = 0xFF00FFFF;
+	uint32_t b_mask = 0xFFFF00FF;
+	int dz = abs(z1 - z0);
+	int red = color >> 24;
+	int green = color >> 16 & 0xFF;
+	int blue = color >> 8 & 0xFF;
 
-    unsigned int xi = *_x;
-    unsigned int yi = *_y;
+	red = red - (dz % 8);// * 0.2;
+	green = green - (dz % 8);// * 0.4;
+	blue = blue - (dz % 8);// * 0.6;
 
-    // Clamp x and y values within the valid range
-    if (*_x < 0)
-        xi = 0;
-    else if (*_x > WIDTH)
-        xi = WIDTH - 1;
+	// printf("red: %x\n", red);
+	// printf("green: %x\n", green);
+	// printf("blue: %x\n", blue);
+	// printf("in color: %x\n", color);
 
-    if (*_y < 0)
-        yi = 0;
-    else if (*_y > HEIGHT)
-        yi = HEIGHT - 1;
+	color = (color & r_mask) | (red << 24);
+	color = (color & g_mask) | (green << 16);
+	color = (color & b_mask) | (blue << 8);
 
-    // Calculate new coordinates if outside the valid range
-    if (xi == 0 || xi == WIDTH - 1)
-        yi = (unsigned int)round(a * xi + b);
-    else if (yi == 0 || yi == HEIGHT - 1)
-        xi = (unsigned int)round((yi - b) / a);
-
-    *_x = xi;
-    *_y = yi;
+	// printf("out color: %x\n", color);
+	return (color);
 }
 
 static void	line_draw(mlx_image_t *img, int x0, int y0, int z0, int x1, int y1, int z1, uint32_t color, ctrl_param_t *control)
 {
-	double dx, dy;
-	double sx = 0;
-	double sy = 0;
-	double e2 = 0;
+	double	dx;
+	double	dy;
+	double	sx;
+	double	sy;
+	double	error;
+	double	e2;
 
 	iso_point(&x0, &y0, &z0, control);
 	iso_point(&x1, &y1, &z1, control);
 
-	// printf("POINT_0  x0 | y0 : %d\t%d\n", x0, y0);
-	// printf("POINT_1  x1 | y1 : %d\t%d\n", x1, y1);
+	// ANDREW'S METHOD - JUST RETURNING WHAT IS OUT OF BOUNDS
+	if (y1 >= HEIGHT)
+		return ;
+	if (y0 >= HEIGHT)
+		return ;
+	if (x0 < 0)
+		return ;
+	else if (y0 < 0)
+		return ;
+	else if (x1 < 0)
+		return ;
+	else if (y1 < 0)
+		return ;
+	else if (x0 >= WIDTH)
+		return ;
+	else if (x1 >= WIDTH)
+		return ;
 
-	double dx_i;
-	double dy_i;
-
-	dx_i = x1 - x0;
-	dy_i = y1 - y0;
+	// BRESENHAM'S LINE DRAWING ALGORITHM
 
 	dx = abs(x1 - x0);
 	dy = -abs(y1 - y0);
 
-	if (y0 > HEIGHT || y0 < 0)
+	sx = -1;
+	if (x0 < x1)
+		sx = 1;
+	sy = -1;
+	if (y0 < y1)
+		sy = 1;
+	error = dx + dy;
+
+while (1)
 	{
-		// printf("POINT_0  x0 | y0 : %d\t%d\n", x0, y0);
-		crop_outside(&x0, &y0, dx_i, dy_i);
-		// printf("CROPPED  x0 | y0 : %d\t%d\n", x0, y0);  // MAYBE I NEED TO PASS THE (x0, y0) values as well
-
-	}
-	if (y1 > HEIGHT || y1 < 0)
-	{
-		// printf("POINT_1  x1 | y1 : %d\t%d\n", x1, y1);
-		crop_outside(&x1, &y1, dx_i, dy_i);
-		// printf("CROPPED  x1 | y1 : %d\t%d\n", x1, y1);
-
-	}
-
-	// BRESENHAM'S LINE DRAWING ALGORITHM
-
-	// ~ NEED TO ADD FILTER TO CUT OFFMAP VALUES! ~
-
-
-
-	sx = x0 < x1 ? 1 : -1;
-	sy = y0 < y1 ? 1 : -1;
-
-  	double error = dx + dy;
-
-	while (1)
-	{
-		mlx_put_pixel(img, x0, y0, color);
-
+		mlx_put_pixel(img, x0, y0, shift_color(color, z0, z1));
 		if (x0 == x1 && y0 == y1)
 			break ;
-
 		e2 = 2 * error;
-	    if (e2 >= dy)
+		if (e2 >= dy)
 		{
 			if (x0 == x1)
 				break ;
@@ -226,12 +182,10 @@ static void	all_keyhooks(mlx_key_data_t keydata, void *param)
 		data->control->transpose_y -= 10;
 	if (keydata.key == MLX_KEY_DOWN && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 		data->control->transpose_y += 10;
-		
 	if (keydata.key == MLX_KEY_PAGE_UP && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 		data->control->rotate -= 0.1;
 	if (keydata.key == MLX_KEY_PAGE_DOWN && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 		data->control->rotate += 0.1;
-	
 	if (keydata.key == MLX_KEY_ESCAPE && (keydata.action == MLX_PRESS || keydata.action == MLX_REPEAT))
 		mlx_close_window(data->mlx);
 }
@@ -240,14 +194,14 @@ static int	render_map(map_data_t *data)
 {
 	int	i;
 	int	j;
-	
+
 	i = 0;
 	while (i < HEIGHT)
 	{
 		j = 0;
 		while (j < WIDTH)
 		{
-			mlx_put_pixel(data->img, j, i, 0x222222FF); 
+			mlx_put_pixel(data->img, j, i, 0x222222FF);
 			j++;
 		}
 		i++;
@@ -256,12 +210,11 @@ static int	render_map(map_data_t *data)
 	while (data->map[i] != NULL)
 	{
 		j = 0;
-		while (j < data->elem - 1) // INSTEAD OF SUBTRACTING ONE POSITION MAYBE IT WOULD BE BETTER TO CLEAN THE TRAILING NULS WITH FT_STRTRIM?
+		while (j < data->elem - 1)
 		{
 			line_draw(data->img, data->control->scale * data->map[i][j].x, data->control->scale * data->map[i][j].y,
-					data->map[i][j].z, data->control->scale * data->map[i][j+1].x, data->control->scale * data->map[i][j+1].y, 
+					data->map[i][j].z, data->control->scale * data->map[i][j+1].x, data->control->scale * data->map[i][j+1].y,
 					data->map[i][j+1].z, data->map[i][j].color, data->control);
-
 			j++;
 		}
 		i++;
@@ -272,10 +225,9 @@ static int	render_map(map_data_t *data)
 		i = 0;
 		while (data->map[i + 1] != NULL)
 		{
-			line_draw(data->img, data->control->scale * data->map[i][j].x, data->control->scale * data->map[i][j].y, 
-					data->map[i][j].z, data->control->scale * data->map[i + 1][j].x, data->control->scale * data->map[i+1][j].y, 
+			line_draw(data->img, data->control->scale * data->map[i][j].x, data->control->scale * data->map[i][j].y,
+					data->map[i][j].z, data->control->scale * data->map[i + 1][j].x, data->control->scale * data->map[i+1][j].y,
 					data->map[i+1][j].z, data->map[i][j].color, data->control);
-
 			i++;
 		}
 		j++;
@@ -305,24 +257,27 @@ int main(int argc, char *argv[])
 
 	if (fd_1 == -1)
 	{
-		perror("Couldn't open file properly.");
+		perror("Couldn't open file properly");
 		exit(1);
 	}
-
 
 
 	// CHECK FILE_prime FORMAT (ALL ROWS HAVE SAME NUMBER OF ELEMENTS)
 	// COUNT LINES IN A FILE_prime
 
 	char *file_second = argv[1];
+	char *tmp_getline;
 	int fd_2 = open(file_second, O_RDONLY);
 
 	int lines_cnt;
 
 	lines_cnt = 0;
-	while (get_next_line(fd_2))
+	while ((tmp_getline = get_next_line(fd_2)))
+	{
 		lines_cnt++;
-
+		free(tmp_getline);
+		// tmp_getline = NULL;
+	}
 	if (close(fd_2) < 0)
 	{
 		perror("Couldn't close the map file properly.");
@@ -335,51 +290,39 @@ int main(int argc, char *argv[])
 
 	map_data_t data;
 
-	
 	data.map = (map_pt_t**) malloc((lines_cnt + 1) * sizeof(map_pt_t*));
 	uint32_t color = 0x55AAFFFF;
 
 	int i;
 	int j;
-	char *get_line = NULL;
+	char *getline;
 	char **line;
 
 	i = 0;
-	get_line = get_next_line(fd_1);
-	if (!get_line)
+	while ((getline = get_next_line(fd_1)))
 	{
-		free(get_line);
-		get_line = NULL;
-	}
-	while (get_line)
-	{
-		line = ft_split(get_line, 32);
+		line = ft_split(getline, 32);
 		if (!line)
 			return (-3);
-		free(get_line);
-		get_line = NULL;
+		free(getline);
+		getline = NULL;
 
 		data.elem = 0;				// SEPARATE FUNCTION COUNT ELEM
 		while (line[data.elem])
 			data.elem++;
 
-		data.map[i] = (map_pt_t*) malloc((data.elem + 1) * sizeof(map_pt_t));
-
+		if (!(data.map[i] = (map_pt_t*) malloc((data.elem) * sizeof(map_pt_t))))
+		{
+			perror("Could not allocate memory for map during parsing:");
+			exit(1);
+		}
 		j = 0;
 		while (line[j])
 		{
 			data.map[i][j].x = j;
 			data.map[i][j].y = i;
-			data.map[i][j].z = ft_atoi(line[j]) * 3;
-
-			// SHIFT COLORS ACCORDING TO Z VALUE
-			
-			// uint8_t red = (color >> 24) & 0xFF;
-			int shift_amount = data.map[i][j].z % 8; // Ensure shift_amount is between 0 and 7
-			// red = (red >> shift_amount) | (red << (8 - shift_amount));
-			// color = (color & 0x55AAFFFF) | (red << 24);
-			data.map[i][j].color = color >> abs(shift_amount/2);
-
+			data.map[i][j].z = ft_atoi(line[j]) * 8;
+			data.map[i][j].color = color;
 
 			free(line[j]);
 			line[j] = NULL;
@@ -388,18 +331,9 @@ int main(int argc, char *argv[])
 		}
 		free(line);
 		line = NULL;
-
-		get_line = get_next_line(fd_1);
-		if (!get_line)
-		{
-			free(get_line);
-			get_line = NULL;
-			break ;
-		}
-
 		i++;
 	}
-	data.map[i] = NULL; // NULL FOR LAST ELEM OF MAP ARRAY
+	data.map[i] = NULL;
 
 	// CLOSE FILE
 
@@ -427,7 +361,7 @@ int main(int argc, char *argv[])
 	data.control = (ctrl_param_t *) malloc(sizeof(ctrl_param_t));
 	data.control->scale = (int)(((sqrt(WIDTH / data.elem) + sqrt(HEIGHT / lines_cnt))) / (WIDTH / HEIGHT * 1.0));
 	printf("CALCULATED SCALE: %d\n", data.control->scale);
-	
+
 	data.control->transpose_x = 0;
 	data.control->transpose_y = 0;
 	data.control->rotate = 1.0;
@@ -454,9 +388,28 @@ int main(int argc, char *argv[])
 
 // TODO:
 
+// - KEYHOOK FOR STRETCHING MODEL
 // - NORM CODE !
-// - COLOR GRADIENT !!
-// - CRASHES AND LEAKS !!!
-// -- REBEL POINTS
+// - COLOR PARSING AND GRADIENT !!
+// - MAKEFILE !!
+// - UNDERSTAND YOUR ALGORITHMS))
+// - SOME STRANGE POINTS AND LINES IN DIFF FILES
+	// because of spaces in the end of lines --> need to parse or trim correctly
+
+// + CRASHES AND LEAKS !!!
+	// DONE!
+
+// +- REBEL POINTS
+	// fixed by just skipping outborder point recalculating
+	// (maybe come up with original cropping solurion later)
+
 // --- SMOOTHER LINES
-// ---- CENTRALED ANCHOR POINT
+	// ???
+// ---- CENTERED ANCHOR POINT
+	// ???
+
+/* PROBLEMS OPENING FILES:
+
+- passing maps dir crashes the program -- make program to take only <.fdf> files
+
+*/
